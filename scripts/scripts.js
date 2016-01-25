766d75d7089356d2dbd0a1fa32c9ace8a6acbe89
Patrick Cameron <patcameron@gmail.com>
2016-01-24 22:09:28 -0500

    Search option checkboxes working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,5 @@
 var storeID = 3;
 
-var searchOptions = '';
-
-var isOnSale = '';
-
-var whereStringResult = '';
-
-var pageNumber = 1;
-
-var app = angular.module('lcboSearch', []);
-
 var filterData = function(data) {
 	for (var i in data.result) {
 		// cut out "Region Not Specified" from search results
@@ -27,18 +17,49 @@ var filterData = function(data) {
 	};
 };
 
+var app = angular.module('lcboSearch', ['ngGeolocation']);
+
+app.controller('getUserLocation', ['$geolocation', '$scope', function($geolocation, $scope) {
+		$geolocation.getCurrentPosition({
+			timeout: 60000
+		}).then(function(position) {
+			$scope.userPosition = position;
+			console.log($scope.userPosition);
+		});
+}]);
+
+
 app.controller('searchController', function($scope) {
-	
+
+	$scope.showResultCount = false;
+
 	$scope.searchAPI = function() {
+
+		var searchOptionsString = [];
+
+		for (var i in $scope.checkboxes) {
+			if ($scope.checkboxes[i].checked === true) {
+				searchOptionsString.push($scope.checkboxes[i].searchCode);
+			}
+		};
+
+		if ($scope.onSale === true) {
+			searchOptionsString.push('has_limited_time_offer');
+			console.log(typeof searchOptionsString);
+		}
+
+		searchOptionsString = searchOptionsString.toString();
+
+		console.log('search options string = ' + searchOptionsString);
+
 		$.ajax({
 		    url: 'http://lcboapi.com/stores/' + storeID + '/products',
 		    dataType: 'jsonp',
 		    method: 'GET',
 		    data: {
-		    	page: pageNumber,
 		        key: lcboKey,
 		        per_page: 100,
-		        where: whereStringResult,
+		        where: searchOptionsString,
 		        q: $scope.search
 		    	}
 		    }).then(function(data) {
@@ -47,82 +68,48 @@ app.controller('searchController', function($scope) {
 		    	$scope.lcbo_data = data.result;
 		    	$scope.totalResults = data.pager.total_record_count;
 		    	$scope.$apply();
-		    	if (data.pager.is_final_page == false) {
-		    		pageNumber++;
-		    		$scope.searchAPI;
-		    	};
+
 		    	$scope.itemsTotal = $('.result').length;
 		    	$scope.itemsInStock = $('.result:visible').length;
+
+		    	$scope.showResultCount = true;
+
 		    	$scope.$digest();
 			});
 	};
 
-	$scope.vqa = function() {
-		if (searchOptions != "is_vqa") {
-			searchOptions = 'is_vqa';
-		} else {
-			searchOptions = '';
-		}
-		console.log('clicked');
-		console.log(searchOptions);
-	};
+	$scope.checkboxes = [{
+          name: 'VQA',
+          searchCode: 'is_vqa',
+          checked: false
+        }, {
+          name: 'Ontario Craft Brew',
+          searchCode: 'is_ocb',
+          checked: false
+        }, {
+          name: 'Kosher',
+          searchCode: 'is_kosher',
+          checked: false
+        }
+    ];
 
-	$scope.ocb = function() {
-		if (searchOptions != "is_ocb") {
-			searchOptions = 'is_ocb';
-		} else {
-			searchOptions = '';
-		}
-	};
+    $scope.onSale = false;
 
-	$scope.kosher = function() {
-		if (searchOptions != "is_kosher") {
-			searchOptions = 'is_kosher';
-		} else {
-			searchOptions = '';
-		}
-	};
+	$scope.updateSelection = function(position, checkboxes) {
+        angular.forEach(checkboxes, function(item, index) {
+          if (position != index) 
+            item.checked = false;
+        });
+      };
 
-	$scope.glutenFree = function() {
-		$scope.search = "gluten free";
-	};
-
-	$scope.onSale = function() {
-		if (isOnSale !== "has_limited_time_offer") {
-			isOnSale = 'has_limited_time_offer';
-		} else if (isOnSale === "has_limited_time_offer") {
-			isOnSale = '';
-		}
-		console.log(isOnSale);
-		$scope.whereString();
-	}
-
-	$scope.whereString = function() {
-		if (searchOptions !== '' && isOnSale !== '') {
-			whereStringResult = searchOptions + ',' + isOnSale
-		} else if (searchOptions === '' && isOnSale !== '') {
-			whereStringResult = isOnSale
-		} else if (searchOptions !== '' && isOnSale === '') {
-			whereStringResult = searchOptions
-		};
-		console.log(whereStringResult);
-		$scope.searchAPI();
-	};
-
-	$scope.reload = function() {
-		$scope.whereString();
-	};
+	$('input.searchCheckbox').on('change', function() {
+	    $('input.searchCheckbox').not(this).prop('checked', false);
+	});
 
 	if ($scope.search === undefined) {
 		$scope.search = suggestedSearches[Math.floor(Math.random()*suggestedSearches.length)];
 	};
 
-	$scope.whereString();
-});
+	$scope.searchAPI();
 
-$(function(){
-	//if a search checkbox is clicked, unselect the others
-	$('input.searchCheckbox').on('change', function() {
-	    $('input.searchCheckbox').not(this).prop('checked', false);  
-	});
 });
