a8231e3527b1b44aa1ef0622d057bb4448360836
Patrick Cameron <patcameron@gmail.com>
2016-01-31 23:22:30 -0500

    improved search, added page routing

--- scripts/localData.js
+++ scripts/localData.js
@@ -1,3 +1,3 @@
 var lcboKey = "MDoyNzc0MTBjNC1iNjQ3LTExZTUtYTZhMy0zMzYxNzI4OTViZWE6UE5NRERuaGFWQXVHQk90cFF2UVdOTUxOOHNvREFxc05mdzha";
 
-var suggestedSearches = ['organic wine', 'organic beer', 'bagnbox', 'whisky ireland', 'Denmark', 'Germany', 'Japan', 'Sicily', 'champagne', 'gluten free', 'South of France', 'soju', 'american pale ale'];
+var suggestedSearches = ['organic wine', 'organic beer', 'bagnbox', 'whisky ireland', 'Denmark', 'Germany', 'Japan', 'Sicily', 'champagne', 'gluten free', 'South of France', 'soju', 'pale ale', 'merlot'];
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,32 @@
-var app = angular.module('lcboSearch', ['ngGeolocation']);
+'use strict';
 
-app.controller('searchController', ['$geolocation', '$scope', function ($geolocation, $scope) {
+var app = angular.module('lcboSearch', ['ngGeolocation', 'ui.router']);
 
+app.config(function ($stateProvider) {
+	$stateProvider.state('index', {
+		url: '',
+		controller: 'mainController',
+		templateUrl: 'scripts/templates/mainController.html'
+	}).state('single', {
+		url: '/:id',
+		controller: 'singleProduct',
+		templateUrl: 'scripts/templates/singleProduct.html'
+	});
+});
+
+app.controller('mainController', ['$geolocation', '$scope', function ($geolocation, $scope) {
+
+	var pageNumber = 1;
 	$scope.nearestStores = [];
-
 	$scope.storeIsFound = false;
-
 	$scope.selectedStoreID;
-
 	$scope.searchOrder = "";
-
 	$scope.showNoResultsMessage = false;
-
-	var lastSearch;
+	var loadMoreResults = false;
+	$scope.store_data = [];
+	$scope.lcbo_data = [];
 
 	// various data filters for raw API data
-
 	var filterData = function(data) {
 
 		var resultsCounter = false;
@@ -27,12 +38,12 @@ app.controller('searchController', ['$geolocation', '$scope', function ($geoloca
 			if (data.result[i].origin.substring(data.result[i].origin.length-0, data.result[i].origin.length-22) == ", Region Not Specified") {
 				data.result[i].origin = data.result[i].origin.slice(0, data.result[i].origin.length-22);
 				data.result[i].origin = data.result[i].origin;
-			};
+			}
 
 			// add spaces around 'x' in quantity information (eg. "2x250 ml --> 2 x 250 ml")
 			if (data.result[i].package !== null && data.result[i].package.charAt(1) == 'x') {
 				data.result[i].package = data.result[i].package.charAt(0) + ' ' + data.result[i].package.charAt(1) + ' ' + data.result[i].package.substring(2);
-			};
+			}
 
 			if (data.result[i].quantity > 0) {
 				resultsCounter = true;
@@ -41,10 +52,8 @@ app.controller('searchController', ['$geolocation', '$scope', function ($geoloca
 			if (data.result[i].secondary_category === "Bags & Boxes") {
 				data.result[i].quantity = 0;
 			}
-
-			// console.log(data.result[i].secondary_category.length);
 			
-		};
+		}
 
 		if (data.pager.total_record_count === 0) {
 			resultsCounter = false;
@@ -60,8 +69,7 @@ app.controller('searchController', ['$geolocation', '$scope', function ($geoloca
 	//use browser location to find closest LCBO stores
 	$geolocation.getCurrentPosition({
 			timeout: 60000
-		}).then(function (position) {
-			console.log(position);
+		}).then(function(position) {
 			$.ajax({
 			    url: 'http://lcboapi.com/stores',
 			    dataType: 'jsonp',
@@ -71,41 +79,41 @@ app.controller('searchController', ['$geolocation', '$scope', function ($geoloca
 			        per_page: 5,
 			        lat: position.coords.latitude,
 			        lon: position.coords.longitude
-			    	}
-			    }).then(function(data) {
-			    	console.log(data);
-			    	$scope.nearestStores = data.result;
-			    	console.log($scope.nearestStores);
-			    	$scope.selectedStoreID = data.result[0].id;
-			    	$scope.storeIsFound = true;
-				    $scope.randomSearch();
-			    	$scope.$digest();
-				});
+			    }
+		    }).then(function(data) {
+		    	$scope.nearestStores = data.result;
+		    	$scope.selectedStoreID = data.result[0].id;
+		    	$scope.storeIsFound = true;
+		    	$('.loadingMoreResultsMessage').addClass('displayNone');
+			    $scope.randomSearch();
+		    	$scope.$digest();
+			});
 		});
 
+	// search a random locally stored keyword
 	$scope.randomSearch = function() {
 		$scope.search = suggestedSearches[Math.floor(Math.random()*suggestedSearches.length)];
 
 		$scope.newSearch();
-	}
+	};
 
 
 	// search store inventory and return results
-
 	$scope.searchStore = function() {
-
-		$scope.store_data = [];
-		$scope.lcbo_data = [];
 
 		$scope.modifyStockCount = false;
 
 		var searchOptionsString = [];
 
+		if ($scope.searchOrder === "limited_time_offer_savings_in_cents.desc") {
+			$scope.onSale = true;
+		}
+
 		for (var i in $scope.checkboxes) {
 			if ($scope.checkboxes[i].checked === true) {
 				searchOptionsString.push($scope.checkboxes[i].searchCode);
 			}
-		};
+		}
 
 		if ($scope.onSale === true) {
 			searchOptionsString.push('has_limited_time_offer');
@@ -114,62 +122,52 @@ app.controller('searchController', ['$geolocation', '$scope', function ($geoloca
 		searchOptionsString = searchOptionsString.toString();
 
 		$.ajax({
-		    url: 'http://lcboapi.com/stores/' + $scope.selectedStoreID + '/products',
-		    dataType: 'jsonp',
-		    method: 'GET',
-		    data: {
-		        key: lcboKey,
-		        per_page: 100,
-		        where: searchOptionsString,
-		        order: $scope.searchOrder,
-		        q: $scope.search
-		    	}
-		    }).then(function(data) {
-		    	console.log(data);
-		    	filterData(data);
-		    	$scope.store_data = data.store;
-		    	for (var i in data.result) {
-					$scope.lcbo_data.push(data.result[i]);
-				};
-		    	$scope.pager_data = data.pager;
-
-		    	$scope.$apply();
-
-		    	$scope.$digest();
-
-		    	if ($scope.pager_data.total_pages > 1) {
-		    		if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-		    		       alert("near bottom!");
-		    		   }
-		    	};
-			});
+	    url: 'http://lcboapi.com/stores/' + $scope.selectedStoreID + '/products',
+	    dataType: 'jsonp',
+	    method: 'GET',
+	    data: {
+	        key: lcboKey,
+	        page: pageNumber,
+	        per_page: 100,
+	        where: searchOptionsString,
+	        order: $scope.searchOrder,
+	        q: $scope.search
+	    	}
+	    }).then(function(data) {
+	    	console.log(data);
+	    	filterData(data);		
+	    	if (data.pager.is_final_page === false) {
+	    		loadMoreResults = true;
+	    		$scope.loadMoreProducts();
+	    	} else if (data.pager.is_final_page === true) {
+	    		loadMoreResults = false;
+	    	}
+	    	$scope.store_data = data.store;
+	    	for (var i in data.result) {
+				$scope.lcbo_data.push(data.result[i]);
+			}
+	    	$scope.$apply();
+	    	$scope.$digest();
+		});
 	};
 
 
-	// automatically update search when new input is entered into search bar
-
+	//reset all params and start new API call when new input is entered into search bar
 	var pendingTask;
-
 	$scope.newSearch = function() {
-
+		$scope.lcbo_data = [];
+		pageNumber = 1;
+		$('.loadingMoreResultsMessage').addClass('displayNone');
 		$scope.showNoResultsMessage = false;
-
 		$('.result').empty();
-		
-		if ($scope.searchOrder === "limited_time_offer_savings_in_cents.desc") {
-			$scope.onSale = true;
-		}
-
 		if (pendingTask) {
 			clearTimeout(pendingTask);
 		}
-
 		pendingTask = setTimeout($scope.searchStore, 800);
 	};
 
 
 	//data for search options checkboxes
-
 	$scope.checkboxes = [{
           name: 'VQA',
           searchCode: 'is_vqa',
@@ -195,12 +193,54 @@ app.controller('searchController', ['$geolocation', '$scope', function ($geoloca
         });
       };
 
-    // $scope.loadMoreResults = function() {
-    // 	$(window).scroll(function() {
-    // 	   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-    // 	       alert("near bottom!");
-    // 	   }
-    // 	});
-    // }
+    //load more products when scrolled to the bottom of the page
+    var moreResultsLoading;
 
+    $scope.loadMoreProducts = function() {
+	    if (loadMoreResults === true) {
+	    	$('.loadingMoreResultsMessage').removeClass('displayNone');
+		    $(window).scroll(function () {
+		            if ($(document).height() <= $(window).scrollTop() + $(window).height()) {
+		                	pageNumber++;
+		                	loadMoreResults = false;
+		                	moreResultsLoading = setTimeout($scope.searchStore, 800);
+		            }
+		    });
+		}
+	};
 }]);
+
+app.controller('singleProduct', ['$scope', function($scope) {
+	$.ajax({
+	    url: 'http://lcboapi.com/products/254946',
+	    dataType: 'jsonp',
+	    method: 'GET',
+	    data: {
+	        key: lcboKey
+	    	}
+	    }).then(function(data) {
+	    	console.log(data);
+	    	$scope.singleData = data;
+		});
+}]);
+
+app.directive('searchBar', function() {
+	return {
+		restrict: 'E',
+		templateUrl: "scripts/templates/searchBar.html"
+	};
+});
+
+app.directive('resultsSection', function() {
+	return {
+		restrict: 'E',
+		templateUrl: "scripts/templates/resultsSection.html"
+	};
+});
+
+app.directive('singleResult', function() {
+	return {
+		restrict: 'E',
+		templateUrl: "scripts/templates/singleSearchResult.html"
+	};
+});
